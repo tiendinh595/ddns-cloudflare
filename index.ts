098b7545b186b0c9311370db008440e0c0d6a0d1
Vu Tien Dinh <tiendinh595@gmail.com>
2023-09-20 11:05:17 +0700

    update dns every 1 minute

--- index.ts
+++ index.ts
@@ -1,14 +1,17 @@
+import cron from 'node-cron';
 let record = 'test.simboutique.vn'
 let email = 'tiendinh595@gmail.com';
 let api_key = '75a17968ba533c716902ca34775baed7d12eb';
 let zone_id = '5d3e2418db6b1bb5131a3e0747333108';
 
 async function getMyIPV4() {
+    console.log('get my ip');
     const response = await fetch("https://checkip.amazonaws.com");
     return await response.text();
 }
 
 async function getIdentifier() {
+    console.log('get identifier')
     const response = await fetch(`https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${record}`, {
         headers: {
             "X-Auth-Email": email,
@@ -21,6 +24,7 @@ async function getIdentifier() {
 }
 
 async function updateDNS(ip: any, identifier: any) {
+    console.log('update dns')
     const response = await fetch(`https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${identifier}`, {
         method: 'PUT',
         headers: {
@@ -35,7 +39,6 @@ async function updateDNS(ip: any, identifier: any) {
         })
     });
     const data = await response.json();
-    console.log(data);
 }
 
 function main() {
@@ -45,12 +48,21 @@ function main() {
         })
         .then(async (ip) => {
             const identifier = await getIdentifier();
-            console.log({ip, identifier})
             return {ip, identifier};
         })
         .then(({ip, identifier}) => {
             return updateDNS(ip, identifier)
         })
+        .catch((err) => {
+            console.log(err);
+        })
+        .finally(() => {
+            console.log('done');
+        })
 }
 
-main()
+// run every 1 minutes
+cron.schedule('*/1 * * * *', () => {
+    console.log('running a task every 1 minutes')
+    main();
+});
