0a570faf4b07d4f2975305fa92654d4580997495
Vu Tien Dinh <tiendinh595@gmail.com>
2023-09-24 14:22:07 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ let api_key = '75a17968ba533c716902ca34775baed7d12eb';
 let configs = [
     {
         zone_id: '5d3e2418db6b1bb5131a3e0747333108',
-        records: ['test.simboutique.vn'],
+        records: [
+            'api.simboutique.vn',
+            'www.api.simboutique.vn',
+            'tools.simboutique.vn',
+            'www.tools.simboutique.vn',
+        ],
     },
 ];
 
@@ -15,7 +20,7 @@ async function getMyIPV4() {
 }
 
 async function getIdentifier(zone_id, record) {
-    console.log('get identifier', {record});
+    console.log('get identifier', { record });
     try {
         const response = await fetch(
             `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${record}`,
@@ -32,11 +37,10 @@ async function getIdentifier(zone_id, record) {
     } catch (error) {
         return null;
     }
-
 }
 
 async function updateDNS(zone_id, record, ip, identifier) {
-    console.log('update dns', {record, ip, identifier});
+    console.log('update dns', { record, ip, identifier });
     const response = await fetch(
         `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${identifier}`,
         {
@@ -66,16 +70,15 @@ async function main() {
             let zoo_id = config.zone_id;
             let identifier = await getIdentifier(zoo_id, record);
             if (identifier) {
-                console.log('found identifier', {record, identifier});
+                console.log('found identifier', { record, identifier });
                 await updateDNS(zoo_id, record, ip, identifier);
             } else {
-                console.log('not found identifier', {record});
+                console.log('not found identifier', { record });
             }
-            console.log('=====================')
+            console.log('=====================');
         }
     }
 }
-
 
 // run every 1 minutes
 cron.schedule('*/1 * * * *', () => {
